6d3d9d7fe8d33f14793260fd5983a7297b0b0662
thouseef-hamza <thousithouseef731@gmail.com>
2024-04-01 11:44:42 +0530

    Vue JS starting & Component

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<!-- Standard Way JS -> HTML -> CSS -->
+
+<!-- JS -->
+<script setup>
+import NestedComponent from './NestedComponent.vue'
+console.log('Hello World')
+</script>
+
+<!-- HTML -->
+<template>
+  <h1>This is a Parent Component</h1>
+  <NestedComponent />
+</template>
+
+<!-- CSS -->
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ src/components/NestedComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Nested Component</h1>
+  </div>
+</template>
